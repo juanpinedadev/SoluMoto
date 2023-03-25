0ed1d706bc68bdaf55a949516f9a135b752375ae
juanpinedadev <juanpineda3000@gmail.com>
2023-03-25 11:51:45 -0500

    Se agregó la clase Producto en la capa de entidades

---
+++ SoluMoto.Entidad/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SoluMoto.Entidad
+{
+    public class Producto
+    {
+        public int IdProducto { get; set; } 
+        public int IdCategoriaProducto { get; set; } //Atributo local
+        public CategoriaProducto CategoriaProducto { get; set; } //Atributo por referencia
+        public string Nombre { get; set; }
+        public string Referencia { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioVenta { get; set; }
+        public decimal PrecioCompra { get; set; }
+        public bool Estado { get; set; } = true;
+        public DateTime FechaRegistro { get; set; }
+    }
+}
