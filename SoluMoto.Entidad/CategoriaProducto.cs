c9423be2e09c900bf565596bf416c19b1ea80c84
juanpinedadev <juanpineda3000@gmail.com>
2023-03-25 11:41:55 -0500

    Se agregó la capa de datos, logica, presentación y se agregó la clase CategoriaProducto en la capa entidad

---
+++ SoluMoto.Entidad/CategoriaProducto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SoluMoto.Entidad
+{
+    public class CategoriaProducto
+    {
+        public int IdCategoriaProducto { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public bool Estado { get; set; } = true;
+        public DateTime FechaRegistro { get; set; } 
+    }
+}
