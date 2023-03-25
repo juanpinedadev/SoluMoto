71753031d8e549d2a7dc0f5a4715ffa0c57a96c5
juanpinedadev <juanpineda3000@gmail.com>
2023-03-25 13:10:25 -0500

    Se agregó la clase Persona en la capa de entidad

---
+++ SoluMoto.Entidad/Persona.cs
@@ -0,0 +1,27 @@
+﻿namespace SoluMoto.Entidad
+{
+    public class Persona
+    {
+        public Persona()
+        {
+        }
+
+        public Persona(int idPersona, string nombreCompleto, string correo)
+        {
+            IdPersona = idPersona;
+            NombreCompleto = nombreCompleto;
+            Correo = correo;
+        }
+
+        public int IdPersona { get; set; }
+        public string NombreCompleto { get; set; }
+        public string Correo { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID: {IdPersona}" +
+                $"\nNombre: {NombreCompleto}" +
+                $"\nCorreo: {Correo}";
+        }
+    }
+}
